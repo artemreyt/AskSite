8839bbde49eaabc5c0dc6ca082140b8f20719978
Corlys velaryon Reichert <creicher@oa-k6.21-school.ru>
2019-11-04 23:22:26 +0300

    trying to do image saving in forms

--- questions/forms.py
+++ questions/forms.py
@@ -2,10 +2,46 @@ from django import forms
 from .models import Question
 
 
-class QuestionForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Напишите здесь название',
-                                                          'width': 300}))
+# class QuestionForm(forms.ModelForm):
+#     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Напишите здесь название',
+#                                                           'width': 300}))
 
-    class Meta:
-        model = Question
-        fields = ['title', 'text', 'photo']
+#     class Meta:
+#         model = Question
+#         fields = ['title', 'text', 'photo']
+
+class AskForm(forms.Form):
+    title = forms.CharField(label='Название:', max_length=255)
+    text = forms.CharField(label="Описание:", widget=forms.Textarea)
+    photo = forms.ImageField(label="Вставьте фотографию:", required=False)
+
+    def clean(self):
+        title = self.data['title']
+        text = self.data['text']
+
+        if len(title.split()) >= len(text.split()):
+            raise forms.ValidationError(
+                u'Описание не может быть меньше, чем название!')
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+
+
+        if len(title.split()) < 3:
+            raise forms.ValidationError(
+                u'В названии должно быть не меньше 3х слов!')
+        return title
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if len(text.split()) < 10:
+            raise forms.ValidationError(
+                u'В описании должно быть не меньше 10 слов')
+        return text
+
+    def save(self):
+        print(self.data)
+        question = Question(**self.cleaned_data)
+        question.save()
+        return question
+
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from questions import models, utils
-from .forms import QuestionForm
+from .forms import AskForm
+from django.conf import settings
+import os.path
 
 def index(request):
     questions = models.Question.objects.published().order_by('datetime_published')
@@ -17,18 +19,23 @@ def ask(request):
     print(request.POST)
 
     if request.method == 'POST':
-        # form = QuestionForm(request.POST, request.FILES)
+        form = AskForm(request.POST, request.FILES)
 
-        form = Question()
         if form.is_valid():
             question = form.save()
+            print(request.POST, request.FILES)
+            upload_image_for_question(request.FILES['photo'])
             # question.author = request.user
-            question.save()
+            # url = question.get_url()
             return success(request)
     else:
-        form = QuestionForm()
+        form = AskForm()
     return render(request, 'questions/new_question.html', {'form': form})
 
+def upload_image_for_question(f):
+    with open(os.path.join(settings.MEDIA_ROOT, f'media/trial.jpg'), 'wb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 def success(request):
     return render(request, 'questions/success.html')
